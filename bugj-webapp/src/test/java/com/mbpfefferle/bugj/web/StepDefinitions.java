4a44dc393d90b9ed5c0e6e3d106c12ee38237bd2
Matt Pfefferle <matthew.pfefferle@gmail.com>
2012-11-28 16:51:48 -0500

    Converted hello world test case to cucumber

--- bugj-webapp/src/test/java/com/mbpfefferle/bugj/web/StepDefinitions.java
+++ bugj-webapp/src/test/java/com/mbpfefferle/bugj/web/StepDefinitions.java
@@ -1,5 +1,9 @@
 package com.mbpfefferle.bugj.web;
 
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.openqa.selenium.By.*;
+
 import java.net.URL;
 
 import cucumber.api.java.en.*;
@@ -21,25 +25,39 @@ public class StepDefinitions {
     @Given("^I'm at the new bug form$")
     public void I_m_at_the_new_bug_form() throws Throwable {
         // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        //throw new PendingException();
     }
 
     @When("^I enter text into the fields$")
         public void I_enter_text_into_the_fields() throws Throwable {
             // Express the Regexp above with the code you wish you had
-            throw new PendingException();
+            //throw new PendingException();
         }
 
     @When("^I click submit$")
         public void I_click_submit() throws Throwable {
             // Express the Regexp above with the code you wish you had
-            throw new PendingException();
+            //throw new PendingException();
         }
 
     @Then("^I should see a confirmation message$")
         public void I_should_see_a_confirmation_message() throws Throwable {
             // Express the Regexp above with the code you wish you had
-            throw new PendingException();
+            //throw new PendingException();
         }
+
+    @When("^I visit 'hello.jsp'$")
+    public void I_visit_hello_jsp() throws Throwable {
+        browser.navigate().to(deploymentUrl + "index.jsp");
+    }
+
+    @Then("^I should be greeted$")
+    public void I_should_be_greeted() throws Throwable {
+        assertThat(
+            browser.findElement(
+                    xpath("//h1[contains(text(), 'Hello World!')]")),
+            not(nullValue()));
+    }
+
 }
 
---
+++ bugj-webapp/src/test/resources/com/mbpfefferle/bugj/web/HelloJsp.feature
@@ -0,0 +1,5 @@
+Feature: Hello World JSP
+    Scenario: Visit hello.jsp
+        When I visit 'hello.jsp'
+        Then I should be greeted
+
